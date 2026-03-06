fae96ffb35dcb094a65f6fc874124a73aa30ded7
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-06 21:14:13 +0700

    update

--- src/components/04.useEffect/TypingTest.jsx
+++ src/components/04.useEffect/TypingTest.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import "./TypingTest.css";
 
 const SAMPLE_TEXT =
   "This endpoint gets you a personalized Donald Trump quote based upon the input that you give it.";
 
 const TypingTest = () => {
   const [userInput, setUserInput] = useState("");
+  const [seconds, setSeconds] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const intervalRef = useRef(null);
+  const inputRef = useRef(null);
 
   const handleChange = (event) => {
     setUserInput(event.target.value);
   };
+
   const renderSampleText = () => {
     return SAMPLE_TEXT.split("").map((char, index) => {
       let className = "";
@@ -25,13 +33,14 @@ const TypingTest = () => {
   return (
     <>
       <h1>Typing Test</h1>
-      <div>{renderSampleText}</div>
+      <div>{renderSampleText()}</div>
       <textarea
         rows={3}
         value={userInput}
         onChange={handleChange}
         placeholder="Start typing..."
       />
+      <p>Time: {seconds.toFixed(2)}</p>
       <p>Text length: {userInput.length}</p>
     </>
   );
