15ebb1c211afb2764f7a826a0b72c2d8983dc782
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-06 20:32:23 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ScrollIntoView from "./components/03.useRef/ScrollIntoView";
 import Timer from "./components/03.useRef/Timer";
 import TimerUseRef from "./components/03.useRef/Timer-useRef";
 import Count from "./components/04.useEffect/Count";
+import Counter from "./components/04.useEffect/Counter";
 import FetchApi from "./components/04.useEffect/FetchApi";
 import Form from "./components/04.useEffect/Form";
 import Like from "./components/04.useEffect/Like";
@@ -27,7 +28,8 @@ const App = () => {
       {/* <Form /> */}
       {/* <FetchApi /> */}
       {/* <Like /> */}
-      <Quote />
+      {/* <Quote /> */}
+      <Counter />
     </div>
   );
 };
---
+++ src/components/04.useEffect/Counter.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `${count} times.`;
+    console.log(`Called useEffect() ${count} times`);
+  }, [count]);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  );
+};
+
+export default Counter;
